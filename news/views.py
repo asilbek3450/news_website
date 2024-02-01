2963be3e395f0ed866fe9956b2dcd6e0465f3f78
asilbek <asilbekmirolimov@gmail.com>
2024-02-01 22:12:26 +0500

    changes

--- news/views.py
+++ news/views.py
@@ -27,11 +27,13 @@ def news_detail(request, pk):
     single_news = News.objects.get(id=pk)  # qaysi yangilikni ichiga kirgan bo'lsa shu yangilikni olish
     breaking_uzb_news = News.objects.filter(category_id__name='O‘ZBEKISTON').order_by('-id')  # yurtimizga doir degan joyga yangiliklarni filtrlab olish
     news_comments = Comment.objects.filter(news_id=single_news)  # shu yangilikga tegishli commentlarni olish
+    trending_news = News.objects.all().order_by('-views')[:3]  # 3 ta bo'lishi kerak shuning uchun [:3]
     context = {
         'single_news': single_news,
         'breaking_uzb_news': breaking_uzb_news,
         'news_comments': news_comments,
-        'categories': categories
+        'categories': categories,
+        'trending_news': trending_news
     }
     return render(request, 'news/single.html', context=context)
 
@@ -40,10 +42,12 @@ def news_category(request, slug):
     categories = NewsCategory.objects.all()
     category = NewsCategory.objects.get(slug=slug)
     news = News.objects.filter(category_id=category)
+    trending_news = News.objects.all().order_by('-views')[:3]  # 3 ta bo'lishi kerak shuning uchun [:3]
     context = {
         'category': category,
         'news': news,
-        'categories': categories
+        'categories': categories,
+        'trending_news': trending_news
     }
     return render(request, 'news/category.html', context=context)
 
